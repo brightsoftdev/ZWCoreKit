6846ac8481687f90e2db47c236c1696e407a1e27
Robert Payne <robertpayne@me.com>
2012-07-16 15:01:57 +1200

    Fix for availability

--- ZWCoreKit/ZWCoreKitAvailability.h
+++ ZWCoreKit/ZWCoreKitAvailability.h
@@ -1,11 +1,17 @@
 #import <Availability.h>
 
-#if (TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR)
-	#define TARGET_SDK_IOS 1
-	#define TARGET_SDK_OSX 0
+#if TARGET_OS_IPHONE
+#define TARGET_SDK_IOS 1
+#define TARGET_SDK_OSX 0
+#elif TARGET_IPHONE_SIMULATOR
+#define TARGET_SDK_IOS 1
+#define TARGET_SDK_OSX 0
+#elif TARGET_OS_MAC
+#define TARGET_SDK_IOS 0
+#define TARGET_SDK_OSX 1
 #else
-	#define TARGET_SDK_IOS 0
-	#define TARGET_SDK_OSX 1
+#define TARGET_SDK_IOS 0
+#define TARGET_SDK_OSX 0
 #endif
 
 #if __has_feature(objc_arc_weak)
